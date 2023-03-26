7227c54332b484726be3cf827fd52c0d2650797d
Shobhit Kushwaha <109756367+shokushwaha@users.noreply.github.com>
2023-03-26 23:34:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const app = express();
-const CHAT_ENGINE_PROJECT_ID = "878e32c8-406f-47a8-8ddc-be8ced18a9bd";
-const CHAT_ENGINE_PRIVATE_KEY = "1c497fab-7dc4-487a-9fdb-993c87660694";
+const CHAT_ENGINE_PROJECT_ID = "";
+const CHAT_ENGINE_PRIVATE_KEY = "";
 
 app.use(express.json());
 app.use(cors({ origin: true }));
